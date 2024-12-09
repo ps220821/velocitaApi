94be72dc2f4c11285f66ecfd4b8e492e5630b715
ps220821 <ps220821@edu.summacollege.nl>
2024-12-09 15:07:05 +0100

    ss

--- obj/Debug/net8.0/velocitaApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/velocitaApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("velocitaApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48db92b7a5ed7aa632b3ee1a545d4020f3ccfc43")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78d3457b6bac6c24d6c0c06d070c13f01dfd0e5c")]
 [assembly: System.Reflection.AssemblyProductAttribute("velocitaApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("velocitaApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
